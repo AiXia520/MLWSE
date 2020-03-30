c870386f983f18a622a185b9ff313b683caa8180
AiXia520 <43028168+AiXia520@users.noreply.github.com>
2020-03-30 12:37:29 +0800

    Update lasso.py

--- lasso.py
+++ lasso.py
@@ -2,55 +2,13 @@ import numpy as np
 import math
 from sklearn.metrics.pairwise import pairwise_distances,cosine_similarity
 import pandas as pd
+
 def softthres(x,e):
     a=np.maximum(x-e,0)
     b=np.maximum(-1*x-e,0)
     return a-b
 
-# def lasso2(X,Y,alpha,beta,gamma,maxIter,miniLossMargin):
-#     n_samples, n_features = X.shape
-#     XTX=X.T*X
-#     XTY=X.T*Y
-#     W_s=(XTX + gamma* np.mat(eyes(n_features))).I*XTY
-#     W_s_1 = W_s
-#     R = pairwise_distances(Y.T, metric="cosine")
-#     iter = 1
-#     oldloss = 0
-#     Lip=math.sqrt(2*math.pow((np.linalg.norm(XTX,ord=2)),2)+math.pow(np.linalg.norm(alpha*R,ord=2),2))
-#
-#     bk=1
-#     bk_1=1
-#
-#     while iter<=maxIter:
-#         W_s_k = W_s + (bk_1 - 1) / bk * (W_s - W_s_1)
-#         Gw_s_k = W_s_k - 1 / Lip * ((XTX * W_s_k - XTY) + alpha * W_s_k * R)
-#         bk_1 = bk
-#         bk = (1 + sqrt(4 * bk ^ 2 + 1)) / 2
-#         W_s_1 = W_s
-#         W_s = softthres(Gw_s_k, beta / Lip)
-#
-#         a = (X*W_s - Y).T
-#         b = X*W_s - Y
-#         predictionLoss = np.trace(a*b)
-#
-#         correlation = np.trace(R*W_s.T*W_s)
-#
-#         sparsity = np.sum(np.sum(np.int64(W_s != 0), axis=0), axis=1)
-#
-#         totalloss = predictionLoss + alpha * correlation + beta * sparsity
-#
-#         if math.fabs(oldloss - totalloss) <= miniLossMargin:
-#             break
-#         elif totalloss <= 0:
-#             break
-#         else:
-#             oldloss = totalloss
-#
-#         iter = iter + 1
-#
-#     return W_s
-
-# 使用近端梯度实现lasso
+#Lasso is implemented using the accelerated proximal gradient 
 def lasso(X,Y,alpha,beta,enta,maxIter,miniLossMargin):
     """
     X:the confidence score matrix
@@ -65,22 +23,20 @@ def lasso(X,Y,alpha,beta,enta,maxIter,miniLossMargin):
     XTX=np.dot(np.transpose(X), X)
     # X.T*Y
     XTY=np.dot(np.transpose(X), Y)
-    #初始化wo,w1
+    #Initialize the wo,w1
     W_s = np.dot(np.linalg.inv(XTX + enta * np.eye(n_features)),XTY).astype(np.float)
     W_s_1 = W_s
-    # 计算相似度距离
+    # Calculate the similarity distance
     H =pairwise_distances(np.transpose(Y),metric="cosine")
-    # L = cosine_similarity(np.transpose(Y))
-    # R=np.diag(L.sum(1))-L
     iter = 1
     oldloss = 0
     df = pd.DataFrame(columns=["step", "loss"])
-    # 计算Lipschitz constant
+    # Colculate Lipschitz constant
     Lip=math.sqrt(2*math.pow((np.linalg.norm(XTX,ord=2)),2)+math.pow(np.linalg.norm(beta*H ,ord=2),2))
-    # 初始化b0,b1
+    # Initialize b0,b1
     bk=1
     bk_1=1
-    # accelerate proximal gradient
+    # the accelerate proximal gradient
     while iter<=maxIter:
         W_s_k = W_s + np.dot((bk_1 - 1) / bk ,(W_s - W_s_1)).astype(np.float)
         Gw_s_k = W_s_k - (1 / Lip) * ((np.dot(XTX , W_s_k) - XTY) + beta * np.dot(W_s_k , H))
@@ -92,15 +48,13 @@ def lasso(X,Y,alpha,beta,enta,maxIter,miniLossMargin):
 
         a=np.transpose(np.dot(X,W_s)-Y)
         b=np.dot(X,W_s)-Y
-        # 计算最小二乘损失
+        # Calculate the least squares loss
         predictionLoss=np.trace(np.dot(a,b))
-        # 计算相关性
+        # Calculate correlation
         correlation=np.trace(np.dot(H,np.dot(np.transpose(W_s),W_s)))
-        # 计算稀疏
+        # Calculate sparsity
         sparsity=np.sum(np.sum(np.int64(W_s!=0),axis=0),axis=1)
-        # correlation=0
-        # sparsity=0
-        # 整体损失
+        # Calculate total loss
         totalloss = predictionLoss + beta * correlation + alpha * sparsity
 
         df = df.append(pd.DataFrame({'step': [iter], 'loss': [math.fabs(oldloss - totalloss)]}))
@@ -113,7 +67,7 @@ def lasso(X,Y,alpha,beta,enta,maxIter,miniLossMargin):
             oldloss = totalloss
 
         iter = iter + 1
-
+    # output iteration loss
     print(df)
     df.to_excel("stacking_iter_loss.xls")
 
