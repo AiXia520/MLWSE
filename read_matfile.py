fea64caef4ca8fa2e24fed11feff1d83079dd834
AiXia520 <43028168+AiXia520@users.noreply.github.com>
2020-03-30 13:19:33 +0800

    Update read_matfile.py

--- read_matfile.py
+++ read_matfile.py
@@ -14,6 +14,9 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
 
+#read mat file for multi-label learning
+#input:path
+#output:X,Y
 def read_matfile(dataset):
 
     path = "data/matfile/" + dataset
@@ -29,68 +32,3 @@ if __name__ == '__main__':
     X,Y=read_matfile(dataset)
     a = np.array(Y.todense())
     print(np.int64(a != 0).sum(0))
-
-    # X_train, y_train, X_test, y_test = iterative_train_test_split(X, Y, test_size=0.3)
-    #
-    # classifier = BinaryRelevance(
-    #     classifier=SVC(probability=True),
-    #     require_dense=[False, True]
-    # )
-    #
-    # # train
-    # classifier.fit(X_train, y_train)
-    #
-    # # predict
-    # predictions = classifier.predict(X_test)
-    # predictions = classifier.predict(X_test)
-    # pro_predictions = classifier.predict_proba(X_test)
-    # print(metrics.hamming_loss(y_test, predictions))
-    i=1
-    k_fold = IterativeStratification(n_splits=5, order=1)
-    for train, test in k_fold.split(X, Y):
-
-        print("fold"+str(i))
-        j_fold = IterativeStratification(n_splits=2, order=1)
-        for new_train, new_test in j_fold.split(X[train], Y[train]):
-            classifier = BinaryRelevance(
-                classifier = SVC(probability=True,C=1.0, kernel='rbf',gamma='scale'),
-                require_dense = [False, True]
-            )
-            print(X[train][new_train])
-            print(Y[train][new_train])
-            print(X[train][new_test])
-            # train
-            classifier.fit(X[train][new_train], Y[train][new_train])
-
-            # predict
-            predictions = classifier.predict(X[train][new_test])
-            predictions = classifier.predict(X[train][new_test])
-            pro_predictions=classifier.predict_proba(X[train][new_test])
-            print(metrics.hamming_loss(Y[train][new_test], predictions))
-
-
-            classifier1 = ClassifierChain(
-                classifier= SVC(probability=True,C=1.0, kernel='rbf',gamma='scale'),
-                require_dense=[False, True]
-            )
-            # train
-            classifier1.fit(X[train][new_train], Y[train][new_train])
-            # predict
-            predictions = classifier1.predict(X[train][new_test])
-            predictions = classifier1.predict(X[train][new_test])
-            pro_predictions=classifier1.predict_proba(X[train][new_test])
-            print(metrics.hamming_loss(Y[train][new_test], predictions))
-
-            classifier2 = ClassifierChain(
-                classifier=RandomForestClassifier(n_estimators=20),
-                require_dense=[False, True]
-            )
-            # train
-            classifier2.fit(X[train][new_train], Y[train][new_train])
-            # predict
-            predictions = classifier2.predict(X[train][new_test])
-            predictions = classifier2.predict(X[train][new_test])
-            pro_predictions=classifier2.predict_proba(X[train][new_test])
-            print(metrics.hamming_loss(Y[train][new_test], predictions))
-
-        i=i+1
